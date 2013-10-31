2b3933f0ef9b1b6bfef179d6b66d74d9b0cdd0f1
Shilpa Modi <shilpa.gundurao@gmail.com>
2013-10-31 12:21:40 -0700

    adding labels for output view

--- calculator/ResultsViewController.h
+++ calculator/ResultsViewController.h
@@ -10,4 +10,12 @@
 
 @interface ResultsViewController : UIViewController
 @property (nonatomic) IBOutlet UILabel* mCurrentLifestyleIncomeLabel;
+@property (nonatomic) IBOutlet UILabel* mAnnualStateTaxesLabel;
+@property (nonatomic) IBOutlet UILabel* mAnnualFederalTaxesLabel;
+@property (nonatomic) IBOutlet UILabel* mStateTaxableIncomeIncomeLabel;
+@property (nonatomic) IBOutlet UILabel* mFederalTaxableIncomeIncomeLabel;
+@property (nonatomic) IBOutlet UILabel* mStateDeductionsLabel;
+@property (nonatomic) IBOutlet UILabel* mFederalDeductionsLabel;
+@property (nonatomic) IBOutlet UILabel* mStateExemptionsLabel;
+@property (nonatomic) IBOutlet UILabel* mFederalExemptionsLabel;
 @end
